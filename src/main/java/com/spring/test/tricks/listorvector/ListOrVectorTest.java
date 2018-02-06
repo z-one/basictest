895444a4c3557af3f2010b905808a00f9d73a7bc
z-one <cheatamerican@gmail.com>
2018-02-07 00:11:54 +0800

    Performance Difference between Arraylist and Vector in Java

---
+++ src/main/java/com/spring/test/tricks/listorvector/ListOrVectorTest.java
@@ -0,0 +1,61 @@
+package com.spring.test.tricks.listorvector;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.CountDownLatch;
+
+import com.spring.test.utils.SleepUtils;
+
+public class ListOrVectorTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("List test");
+        List<String> list = new ArrayList<String>(5000);
+        testList(list);
+        
+        System.out.println("================");
+        System.out.println("Vector test");
+        List<String> vector = new Vector<String>(5000);
+        testList(vector);
+        
+        System.out.println("================");
+        System.out.println("Linked test");
+        List<String> Linkedlist = new LinkedList<String>();
+        testList(Linkedlist);
+    }
+    
+    private static void testList(final List<String> list)
+    {
+        long start = System.currentTimeMillis();
+        
+        final CountDownLatch latch = new CountDownLatch(5000);
+        
+        for (int i = 0; i < 5000; i++)
+        {
+//            new Thread(new Runnable()
+//            {
+//                @Override
+//                public void run()
+//                {
+                    list.add(System.currentTimeMillis() + "i");
+                    //SleepUtils.second(2);
+                    latch.countDown();
+//                }
+//            }).start();
+        }
+        
+        try
+        {
+            latch.await();
+        }
+        catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+        }
+        
+        System.out.println(list.getClass() + "耗时：" + (System.currentTimeMillis() - start) + " 总量：" + list.size());
+    }
+}
